003220cafc079c222e460056e978570dee7198c9
Zahid <zahidullah09052000@gmail.comm>
2022-04-07 22:15:40 +0500

    array methods

--- arr.js
+++ arr.js
@@ -1,7 +1,23 @@
-const arr = [23, 34, 56, 90];
-
-const arr2 = [24, 99, 73, 56];
-
-const newArr = arr.concat(arr2);
-
-console.log(newArr);
+const arr = [32, 45, 56, 67]
+    // iteration
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
+// forEach
+arr.forEach(function(item, index) {
+        console.log(item, index)
+    })
+    // map
+const newArr = arr.map(function(item, index) {
+    return item * 2
+})
+console.log(newArr)
+    // filter
+const filterArr = arr.filter(function(item, index) {
+    return item > 50
+})
+console.log(filterArr)
+    // find
+const findArr = arr.find(function(item, index) {
+    return item > 50
+})
