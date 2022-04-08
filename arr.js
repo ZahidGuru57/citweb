b3fb9802a39dad3a9b5ffe5cbcf3503b73ef6146
Zahid <zahidullah09052000@gmail.comm>
2022-04-09 01:47:31 +0500

    variable change

--- arr.js
+++ arr.js
@@ -1,23 +1,23 @@
 const arr = [32, 45, 56, 67]
-    // iteration
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i])
+const [first, second, third, fourth] = arr
+
+console.log(first);
+
+const student = {
+
+    name: "John",
+    age: 25,
+    isActive: true,
+    address: {
+        city: "Hangu",
+        country: "Pakistan"
+    }
 }
-// forEach
-arr.forEach(function(item, index) {
-        console.log(item, index)
-    })
-    // map
-const newArr = arr.map(function(item, index) {
-    return item * 2
-})
-console.log(newArr)
-    // filter
-const filterArr = arr.filter(function(item, index) {
-    return item > 50
-})
-console.log(filterArr)
-    // find
-const findArr = arr.find(function(item, index) {
-    return item > 50
-})
+
+const { name, address } = student;
+const { city, country } = address;
+
+console.log(name)
+console.log(address);
+
+console.log(city);
